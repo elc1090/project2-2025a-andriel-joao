1067c9d3a03f088aa7da41415d15269e2467f578
jpsousas <joao.sousa@acad.ufsm.br>
2025-04-21 00:27:54 -0300

    navbar funcionando, falta estilizar.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 //import logo from './logo.svg';
 import SimpleSlider from './utils/SliderBanner';
+import ResponsiveAppBar from './utils/NavBar';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <ResponsiveAppBar/>
         <SimpleSlider/>
       </header>
     </div>
--- src/utils/SliderBanner.js
+++ src/utils/SliderBanner.js
@@ -8,21 +8,24 @@ export default function SimpleSlider() {
   var settings = {
     dots: true,
     infinite: true,
-    speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
-    adaptiveHeight: true
+    autoplay: true,
+    autoplaySpeed: 2000,
+    pauseOnHover: true,
+    centerMode: true,
+    useCSS: true
   };
   return (
-    <Slider {...settings}>
+    <Slider {...settings} style={{padding: "2em"}}>
       <div>
-        <img style={{height: 400, width: 1400}} className="imagemHeader" src={pessoasTreinando}/>
+        <img style={{height: 400, width: 1400}} className="imagemHeader" src={pessoasTreinando} alt="Pessoas treinando"/>
       </div>
       <div>
-        <img style={{height: 400, width: 1400}} className="imagemHeader" src="https://www.gsuplementos.com.br/upload/banner/629d18b9f32e78d53c8a3418f7e197ba.webp"/>
+        <img style={{height: 400, width: 1400}} className="imagemHeader" src="https://www.gsuplementos.com.br/upload/banner/629d18b9f32e78d53c8a3418f7e197ba.webp" alt="Divulgacao creatina"/>
       </div>
       <div>
-        <img style={{height: 400, width: 1400}} src="https://www.gsuplementos.com.br/upload/banner/5d8e0c4d9d5ae8dd46edcf203b0458fc.webp"/>
+        <img style={{height: 400, width: 1400}} src="https://www.gsuplementos.com.br/upload/banner/5d8e0c4d9d5ae8dd46edcf203b0458fc.webp" alt="Divulgacao hipercalorico"/>
       </div>
     </Slider>
   );
